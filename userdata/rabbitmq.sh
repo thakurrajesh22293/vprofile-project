95f62cc96b452e5a51bb09033be0ee8939891d64
thakurrajesh22293 <139438293+thakurrajesh22293@users.noreply.github.com>
2026-07-16 18:23:16 +0530

    Update rabbitmq.sh

--- userdata/rabbitmq.sh
+++ userdata/rabbitmq.sh
@@ -1,60 +1,54 @@
 #!/bin/bash
+
 set -e
 
 echo "===== Updating system ====="
-sudo dnf update -y
 
-echo "===== Fixing minimal package conflicts ====="
-sudo dnf install -y curl gnupg2 wget --allowerasing
+sudo apt update
+sudo apt upgrade -y
 
-echo "===== Adding Erlang repository ====="
-sudo tee /etc/yum.repos.d/rabbitmq-erlang.repo > /dev/null <<EOF
-[rabbitmq-erlang]
-name=RabbitMQ Erlang
-baseurl=https://packagecloud.io/rabbitmq/erlang/el/9/x86_64
-enabled=1
-gpgcheck=0
-repo_gpgcheck=0
-sslverify=1
-EOF
+echo "===== Installing prerequisites ====="
+
+sudo apt install -y curl gnupg apt-transport-https lsb-release ca-certificates
 
 echo "===== Installing Erlang ====="
-sudo dnf install -y erlang
 
-echo "===== Adding RabbitMQ repository ====="
-sudo tee /etc/yum.repos.d/rabbitmq.repo > /dev/null <<EOF
-[rabbitmq]
-name=RabbitMQ Server
-baseurl=https://packagecloud.io/rabbitmq/rabbitmq-server/el/9/x86_64
-enabled=1
-gpgcheck=0
-repo_gpgcheck=0
-sslverify=1
-EOF
+sudo apt install -y erlang
 
 echo "===== Installing RabbitMQ ====="
-sudo dnf install -y rabbitmq-server
 
-echo "===== Enabling & starting RabbitMQ ====="
+sudo apt install -y rabbitmq-server
+
+echo "===== Enabling RabbitMQ ====="
+
 sudo systemctl enable rabbitmq-server
 sudo systemctl start rabbitmq-server
 
 echo "===== Configuring RabbitMQ ====="
-sudo mkdir -p /etc/rabbitmq
-sudo tee /etc/rabbitmq/rabbitmq.conf > /dev/null <<EOF
+
+sudo tee /etc/rabbitmq/rabbitmq.conf >/dev/null <<EOF
 loopback_users.none = true
 EOF
 
 sudo systemctl restart rabbitmq-server
 
-echo "===== Creating RabbitMQ user ====="
+echo "===== Creating user ====="
+
 sudo rabbitmqctl add_user test test || true
 sudo rabbitmqctl set_user_tags test administrator
 sudo rabbitmqctl set_permissions -p / test ".*" ".*" ".*"
 
-echo "===== Enabling RabbitMQ Management UI ====="
+echo "===== Enabling Management Plugin ====="
+
 sudo rabbitmq-plugins enable rabbitmq_management
 
-echo "===== RabbitMQ installation completed successfully ====="
-echo "UI: http://<EC2-PUBLIC-IP>:15672"
-echo "User: test | Password: test"
+sudo systemctl restart rabbitmq-server
+
+echo "======================================"
+echo "RabbitMQ Installed Successfully"
+echo "======================================"
+echo "Management UI:"
+echo "http://<SERVER-IP>:15672"
+echo
+echo "Username: test"
+echo "Password: test"
