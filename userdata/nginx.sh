583e7461b608ef96f83b6c44ea1e42469a56496f
thakurrajesh22293 <139438293+thakurrajesh22293@users.noreply.github.com>
2026-07-17 19:57:30 +0530

    Update nginx.sh

--- userdata/nginx.sh
+++ userdata/nginx.sh
@@ -1,32 +1,36 @@
-# adding repository and installing nginx		
+# Update package list and install Nginx
 apt update
 apt install nginx -y
+
+# Create Nginx configuration
 cat <<EOT > vproapp
 upstream vproapp {
-
- server app01:8080;
-
+    server 127.0.0.1:8080;
 }
 
 server {
+    listen 80;
 
-  listen 80;
-
-location / {
-
-  proxy_pass http://vproapp;
-
+    location / {
+        proxy_pass http://vproapp;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+    }
 }
-
-}
-
 EOT
 
+# Move configuration
 mv vproapp /etc/nginx/sites-available/vproapp
-rm -rf /etc/nginx/sites-enabled/default
-ln -s /etc/nginx/sites-available/vproapp /etc/nginx/sites-enabled/vproapp
 
-#starting nginx service and firewall
-systemctl start nginx
+# Remove default site and enable new site
+rm -f /etc/nginx/sites-enabled/default
+ln -sf /etc/nginx/sites-available/vproapp /etc/nginx/sites-enabled/vproapp
+
+# Test configuration
+nginx -t
+
+# Start and enable Nginx
 systemctl enable nginx
 systemctl restart nginx
